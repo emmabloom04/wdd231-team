84b773ed95ceccc88c051bffda880ca2475e187c
emmabloom04 <blo23006@byui.edu>
2025-06-03 16:47:32 -0600

    creating basic html with hardcoded songs

--- src/js/main.js
+++
@@ -1,5 +0,0 @@
-const p = document.querySelector(".test-p");
-const button = document.querySelector(".test-button");
-button.addEventListener("click", (event) => {
-  p.textContent = "This text is different now";
-});
